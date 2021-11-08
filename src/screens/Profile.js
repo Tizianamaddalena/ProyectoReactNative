674e8692f16effbbf5e72ed43a12c77a237c8285
Tiziana <tizimb@gmail.com>
2021-11-08 16:17:26 -0300

    se ve el usuario en el perfil

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { auth } from '../firebase/config';
 
 export default function Profile(props){
     return(
         <View style={styles.container}>
-            <Text>Perfil</Text>
+            <Text>Usuario</Text>
+            <Text>{auth.currentUser.displayName}</Text>
+
+            <Text>Email</Text>
             <Text>{props.user}</Text>
+
 
             <TouchableOpacity onPress={()=> props.signOut()} //como signOut es una funcion no se pone this
             >

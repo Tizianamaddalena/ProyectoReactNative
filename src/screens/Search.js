3a18ee406ea57c79402332d3265f9dfee3ac6e0c
Iara <iburgueno@udesa.edu.ar>
2021-11-19 18:00:11 -0300

    creo screen del buscador

---
+++ src/screens/Search.js
@@ -0,0 +1,23 @@
+import React, {Component} from "react";
+import {View, Text, TextInput, TouchableOpacity, StyleSheet, Image} from 'react-native';
+
+class Seach extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            posts: [],
+            loadingPosts: true;
+        }
+    }
+ 
+    render(){
+        return(
+            <View style= {styles.container}>
+                
+               
+            </View>
+        )
+    }
+}
+
+export deafult Search;
